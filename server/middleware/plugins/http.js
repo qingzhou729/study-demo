a05726c63e6c9e8b83d2e7928ff94dc15d6a1cc1
yuxue.yang <yuxue.yang@husor.com>
2020-04-08 17:23:30 +0800

    demo

--- server/middleware/plugins/http.js
+++ server/middleware/plugins/http.js
@@ -7,7 +7,7 @@ module.exports = () => {
 
         // 这个响应头的意义在于，设置一个相对时间，在该非简单请求在服务器端通过检验的那一刻起，
         // 当流逝的时间的毫秒数不足Access-Control-Max-Age时，就不需要再进行预检，可以直接发送一次请求。
-        ctx.set('Access-Control-Max-Age', 3600 * 24);
+        // ctx.set('Access-Control-Max-Age', 3600 * 24);
         if (ctx.method == 'OPTIONS') {
             ctx.body = 200; 
         } else {
