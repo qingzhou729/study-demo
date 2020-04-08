d99ac3664816670913de7917140f65e28784499e
yuxue.yang <yuxue.yang@husor.com>
2020-04-08 17:33:59 +0800

    max-age

--- server/middleware/plugins/http.js
+++ server/middleware/plugins/http.js
@@ -7,11 +7,8 @@ module.exports = () => {
         ctx.set('Access-Control-Allow-Credentials', true);
         ctx.set('Access-Control-Allow-Headers', 'content-type');
         ctx.set('Access-Control-Allow-Methods', 'OPTIONS, GET, HEAD, PUT, POST, DELETE, PATCH');
-        ctx.set('Etag', hash);
-
-        // 这个响应头的意义在于，设置一个相对时间，在该非简单请求在服务器端通过检验的那一刻起，
-        // 当流逝的时间的毫秒数不足Access-Control-Max-Age时，就不需要再进行预检，可以直接发送一次请求。
-        // ctx.set('Access-Control-Max-Age', 3600 * 24);
+        ctx.set('Access-Control-Max-Age', 3600 * 24);
+         ctx.set('Etag', hash);
         if (ctx.method == 'OPTIONS') {
             ctx.body = 200; 
         } else {
