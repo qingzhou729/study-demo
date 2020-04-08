dadf83a94904d4ece86d0f715118ae2e4eaf8a81
yuxue.yang <yuxue.yang@husor.com>
2020-04-08 17:31:30 +0800

    etag

--- server/middleware/plugins/http.js
+++ server/middleware/plugins/http.js
@@ -1,9 +1,13 @@
+var hashStr = "A hash string.";
+var hash = require("crypto").createHash('sha1').update(hashStr).digest('base64');
+
 module.exports = () => {
     return async (ctx, next) => {
         ctx.set('Access-Control-Allow-Origin', 'http://test.xue.com');
         ctx.set('Access-Control-Allow-Credentials', true);
         ctx.set('Access-Control-Allow-Headers', 'content-type');
         ctx.set('Access-Control-Allow-Methods', 'OPTIONS, GET, HEAD, PUT, POST, DELETE, PATCH');
+        ctx.set('Etag', hash);
 
         // 这个响应头的意义在于，设置一个相对时间，在该非简单请求在服务器端通过检验的那一刻起，
         // 当流逝的时间的毫秒数不足Access-Control-Max-Age时，就不需要再进行预检，可以直接发送一次请求。
