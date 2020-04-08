cf4878cadb223ba45099cec27c80fc86a61e2e0a
yuxue.yang <yuxue.yang@husor.com>
2020-04-08 20:50:59 +0800

    test

--- server/middleware/plugins/http.js
+++ server/middleware/plugins/http.js
@@ -3,11 +3,11 @@
 
 module.exports = () => {
     return async (ctx, next) => {
-        ctx.set('Access-Control-Allow-Origin', 'http://test.xue.com');
+        // ctx.set('Access-Control-Allow-Origin', 'http://test.xue.com');
         ctx.set('Access-Control-Allow-Credentials', true);
         ctx.set('Access-Control-Allow-Headers', 'content-type');
         ctx.set('Access-Control-Allow-Methods', 'OPTIONS, GET, HEAD, PUT, POST, DELETE, PATCH');
-        ctx.set('Cache-Control', 'public, max-age=60');
+        // ctx.set('Cache-Control', 'public, max-age=60');
         //  ctx.set('Etag', hash);
         if (ctx.method == 'OPTIONS') {
             ctx.body = 200; 
