63b1170bf8142b5385195726c9ec76190f0539ed
yuxue.yang <yuxue.yang@husor.com>
2020-04-23 20:42:29 +0800

    test

--- server/index.js
+++ server/index.js
@@ -1,15 +1,13 @@
+// demo 1
 const Koa = require('koa');
 const static = require('koa-static');
-
-const etag = require('koa-etag');
-const conditional = require('koa-conditional-get');
-
 const path = require('path');
+const conditional = require('koa-conditional-get');
+const etag = require('koa-etag');
 const app = new Koa();
 
 app.use(conditional());
 app.use(etag());
-
 const staticPath = './static';
 app.use(static(
     path.join( __dirname, staticPath), {
@@ -21,59 +19,3 @@ const PORT = 8001;
 app.listen(PORT);
 console.log(`server is starting at port ${PORT}`);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const Koa = require('koa');
-// // const middleware = require('./middleware');
-// // const session = require("koa-session2");
-// // const Store = require("./utils/Store.js");
-// // const body = require('koa-body');
-// const etag = require('koa-etag');
-// const conditional = require('koa-conditional-get');
-// const static = require('koa-static');
-// const path = require('path');
-// const app = new Koa();
-
-// // session配置
-// // app.use(session({
-// //     store: new Store(),
-// //     key: "SESSIONID",
-// // }));
-// // 解析 post 参数
-// // app.use(body());
-// // app.use(conditional());
-// // app.use(etag());
-// // 中间件
-// // middleware(app);
-// const staticPath = './static'
-// app.use(static(
-//     path.join( __dirname, staticPath), {
-//     maxage: 10 * 1000,
-// }));
-
-// const PORT = 8001;
-// // 服务启动
-// app.listen(PORT);
-// console.log(`server is starting at port ${PORT}`);
--- server/static/index.js
+++ server/static/index.js
@@ -1,1 +1,24 @@
-console.log('浏览器缓存测试');
+// demo1 promise
+// let p1 = new Promise(() => {
+//     setTimeout(() => {
+//         console.log(1);
+//     }, 1000);
+//     console.log(2);
+// });
+// console.log(3); // 2 3 1
+
+// demo2 promise
+let p2 = new Promise((resolve, reject) => {
+    console.log(1);
+    resolve('浪里行舟')
+    console.log(2)
+});
+
+// then:设置成功或者失败后处理的方法
+p2.then(result => {
+    //p1延迟绑定回调函数
+    console.log('成功 '+ result);
+},  reason => {
+   console.log('失败 '+ reason);
+});
+console.log(3);
